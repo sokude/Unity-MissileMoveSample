aaac5796d2766725e692bbd777ed40926110dbe9
Seiki Okude <sokude@gmail.com>
2015-09-26 03:42:29 +0900

    StandardAssetsのUtilitieとParticleSystemをImportしてください。 実行して、マウス左クリックで弾発射です。

---
+++ Assets/missileMoveScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class missileMoveScript : MonoBehaviour {
+
+    public Transform expl;
+
+    Transform targetEnemy;
+    float turnSpeed = 5;
+    float forwardSpeed = 0;
+    public Transform TargetEnemy
+    {
+        set {
+            targetEnemy = value;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 myDirc = transform.TransformDirection(Vector3.forward);
+        Vector3 toEnemy = targetEnemy.position - transform.position;
+        Vector3 cv = Vector3.Cross(myDirc, toEnemy.normalized);
+        if (cv.y > 0)
+        {
+            transform.Rotate(new Vector3(0, 1, 0), turnSpeed);
+        }
+        else if(cv.y < 0)
+        {
+            transform.Rotate(new Vector3(0, 1, 0), -turnSpeed);
+        }
+        forwardSpeed += Time.deltaTime * 100f;
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+
+    }
+
+    void OnTriggerEnter(Collider _other)
+    {
+        if(_other.tag == "Enemy")
+        {
+            Instantiate(expl,transform.position,Quaternion.identity);
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ Assets/playerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+    float shotinterval = 0;
+    public Transform missilePrefab;
+    public Transform enemy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (shotinterval <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                for (int i = 0; i < 5; ++i)
+                {
+                    Quaternion qqq = Quaternion.Euler(0, i * 40 -80, 0);
+                    Transform msl = Instantiate(missilePrefab, transform.position + new Vector3(i*2 - 4,0,0), qqq) as Transform;
+                    msl.GetComponent<missileMoveScript>().TargetEnemy = enemy;
+                }
+                shotinterval = 8;
+            }
+        }
+        else
+        {
+            shotinterval -= 1;
+        }
+
+    }
+}
